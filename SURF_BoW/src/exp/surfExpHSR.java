cbbea1a012678012ee1dc537a00c99601cdfd85a
Jivko Sinapov <jsinapov@gmail.com>
2017-11-19 18:09:17 -0500

    added script for computing the bag of words representation for the HSR dataset

---
+++ SURF_BoW/src/exp/surfExpHSR.java
@@ -0,0 +1,234 @@
+package exp;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import utils.SurfUtils;
+import weka.clusterers.Cobweb;
+import weka.clusterers.SimpleKMeans;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Utils;
+
+public class surfExpHSR {
+
+	public static void buildClustering(int num_trials, int num_objects, String [] behaviors, String img_topic,
+			String surf_data_path, double sample_fraction, int num_visual_words, String kmeans_file){
+		
+		ArrayList<double[]> features_kmeans = new ArrayList<double[]>();
+		
+		for (int t = 1; t <= num_trials; t++){
+			System.out.println("Loading features from trial "+t);
+			for (int o = 1; o <= num_objects; o++){
+				for (int b = 0; b < behaviors.length; b++){
+					String surf_file_name = new String(surf_data_path+"/"+"obj"+o+"_"+img_topic+"_trial"+t+"_"+behaviors[b]+".surf.txt");
+					
+					ArrayList<double[]> features_i = SurfUtils.loadSubsetFeaturesFromFile(surf_file_name, sample_fraction);
+					//System.out.println("loaded "+features_i.size()+" surf features");
+					features_kmeans.addAll(features_i);
+				}
+			}
+			
+		}
+		
+		//Step 2: apply k-means
+		Instances weka_data = SurfUtils.toInstances(features_kmeans);
+		
+		System.out.println("Loaded a total of "+weka_data.numInstances() +" datapoints for k-means");
+		
+		
+		try {
+			SimpleKMeans C = new SimpleKMeans();
+			C.setNumClusters(num_visual_words);
+			
+			//Cobweb C = new Cobweb();
+			
+			C.setDebug(true);
+			System.out.println("Building Clustering...");
+			C.buildClusterer(weka_data);
+			System.out.println("Done. Found "+C.numberOfClusters() + " clusters");
+			
+			System.out.println(C.toString());
+			
+			FileOutputStream fout;
+			fout = new FileOutputStream(kmeans_file);
+			ObjectOutputStream oos = new ObjectOutputStream(fout);
+			oos.writeObject(C);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	public static void computeBoW(int num_trials, int num_objects, String [] behaviors, String img_topic, String kmeans_file, 
+			String output_path, String surf_data_path, int num_visual_words, boolean normalize){
+		//use clustering to compute BoW representation
+		InputStream file;
+		try {
+					
+			//load the clustering
+			file = new FileInputStream(kmeans_file);
+			InputStream buffer = new BufferedInputStream(file);
+			ObjectInput input = new ObjectInputStream (buffer);
+			
+			
+			SimpleKMeans C = (SimpleKMeans)input.readObject();
+
+			//for each interaction, compute and save distribution of visual words
+			for (int b = 0; b < behaviors.length; b++){
+				System.out.println("Processing behavior "+behaviors[b]);
+						
+				String norm_tag = "";
+				if (normalize)
+					norm_tag = new String("-normalized");
+				String outfile_b = new String(output_path+"/"+behaviors[b]+"_surf"+num_visual_words+"-"+img_topic+""+norm_tag+".csv");
+						
+				//open a file to save the results
+				FileWriter FW = new FileWriter(outfile_b);
+						
+				for (int o = 1; o <= num_objects; o++){
+					for (int t = 1; t <= num_trials; t++){
+								
+						String surf_file_name = new String(surf_data_path+"/"+"obj"+o+"_"+img_topic+"_trial"+t+"_"+behaviors[b]+".surf.txt");
+						
+						
+						//the features for the interaction o b t
+						ArrayList<double[]> features = SurfUtils.loadFeaturesFromFile(surf_file_name);
+								
+						//conts for visual words
+						double [] count = new double[num_visual_words];
+						for (int i = 0; i < count.length; i++)
+							count[i]=0;
+								
+						//compute the counts
+						for (int i = 0; i < features.size(); i++){
+							Instance x_i = SurfUtils.toInstance(features.get(i));
+							int cluster_x = C.clusterInstance(x_i);
+							count[cluster_x]++;
+						}
+						if (normalize)
+							Utils.normalize(count);
+								
+						FW.write(new String(o+","));
+						FW.write(new String(t+","));
+						for (int i = 0; i < count.length; i++){
+							if (i < count.length-1)
+								FW.write(new String(count[i]+","));
+							else
+								FW.write(new String(count[i]+"\n"));
+						}
+								
+						//save the counts
+					}
+				}
+			FW.close();
+						
+			}
+					
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num_trials = 1;
+		int num_objects = 32;
+		
+		String [] behaviors = {"look","grasp","lift","hold","shake","lower","drop","push_side","press_top_slow","press_top_fast"};
+		
+		double sample_fraction = 0.002; // what proportion of features to use for learning clustering
+		int num_visual_words = 200;
+		
+		
+		String surf_data_path = "/home/jsinapov/research/datasets/hsr_data/surf_features/";
+		String output_path = "/home/jsinapov/research/datasets/hsr_data/surf_bow/";
+		String kmeans_file = new String(surf_data_path+"/kmeans_k"+num_visual_words+".obj");
+		
+		String topic = "headasus";
+				
+		//System.out.println("num args = "+args.length);
+		//System.out.println(args[0]);
+		
+		buildClustering(num_trials,num_objects, behaviors, topic, surf_data_path,sample_fraction,num_visual_words, kmeans_file);
+		
+		computeBoW(num_trials, num_objects, behaviors, topic, kmeans_file, 
+				output_path, surf_data_path, num_visual_words, false);
+		
+		/*if (args[0].equals("build")){
+			System.out.println("Build command");
+			surf_data_path = args[1];
+			kmeans_file = args[2];
+			
+			num_visual_words = Integer.parseInt(args[3]);
+			sample_fraction = Double.parseDouble(args[4]);
+
+			
+			//build clustering -- comment this line out once it's built
+			buildClustering(num_trials,num_objects, behaviors, topic, surf_data_path,sample_fraction,num_visual_words, kmeans_file);
+			
+		}
+		else if (args[0].equals("process")){
+			System.out.println("Process command");
+			surf_data_path = args[1];
+			kmeans_file = args[2];
+			output_path = args[3];
+			num_visual_words = Integer.parseInt(args[4]);
+			
+			
+			System.out.println("Surf data path = "+surf_data_path);
+			System.out.println("k-means file: "+kmeans_file);
+			System.out.println("output path = "+output_path);
+			
+			//compute BoW representation  -- comment this line in once the clustering has been built and saved
+			computeBoW(num_trials, num_objects, behaviors, kmeans_file, 
+					output_path, surf_data_path, num_visual_words, false);
+			
+		}
+		else if (args[0].equals("process_norm")){
+			surf_data_path = args[1];
+			kmeans_file = args[2];
+			output_path = args[3];
+			num_visual_words = Integer.parseInt(args[4]);
+			
+			//compute BoW representation  -- comment this line in once the clustering has been built and saved
+			computeBoW(num_trials, num_objects, behaviors, kmeans_file, 
+					output_path, surf_data_path, num_visual_words, true);
+			
+		}*/
+		
+		
+		
+		
+		
+	   
+	}
+
+}
